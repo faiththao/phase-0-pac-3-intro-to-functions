7bcbd8644a1a57aeacbf49df5d71c929e3afaad9
Faith <thao.faith2@gmail.com>
2021-06-19 10:37:40 -0600

    New index

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log('Hello!');
+}
+sayHello();
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  } 
+
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  } 
+
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo('Isabel'); 
+  sayHelloTo('Jane'); 
+
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }   
+  say("Goodbye", "Julio");
+  say("Julio", "hello"); 
+
+
+  function add(x, y) {
+    return x + y;
+  } 
+  console.log(add(1,2)); 
+
+  function say(greeting, firstName) {
+    console.log('I was called!');
+    return `${greeting}, ${firstName}!`;
+  } 
+
+  function say(greeting, firstName) {
+      console.log("Hi", "Faith");
+      return `${greeting}, ${firstName}!`;
+  }
